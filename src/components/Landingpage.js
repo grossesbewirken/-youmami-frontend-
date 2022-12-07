1bdc049142c9a6a60cc8e58e15fee149f79a9ef8
BreffJaun <braun_jeff@web.de>
2022-12-07 20:28:46 +0100

    Martin & Jeff

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -8,17 +8,13 @@ const Landingpage = ({recipesData}) => {
       <div className="center">
         <h1>Are you hungry</h1>
         <h3>get inspired</h3>
-      </div>
-
-
-      {recipe && <div className="description">
+      </div>recipe && <div className="description">
           <h1 className="rezept">{recipe.name}</h1>
           <img src={recipe.img} alt={recipe.name} />
           <h3>{recipe.description}</h3>
           <p className="Beschreibung">{recipe.description}</p>
           <button className="collapsible">+</button>
         </div>
-        <h1>TEST</h1>
     </div>
   );
 };
