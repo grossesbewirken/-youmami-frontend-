35bae3ddcf4b0d8de91d18108db5aabef04e0e55
BreffJaun <braun_jeff@web.de>
2022-12-08 13:21:15 +0100

    Bereinigungen

--- src/components/OneRecipeBig.js
+++ src/components/OneRecipeBig.js
@@ -11,24 +11,6 @@ import { HiMagnifyingGlass } from "react-icons/hi"
 import { AiOutlineCloseCircle } from "react-icons/ai"
 import { GiFireworkRockete } from "react-icons/gi"
 
-
-
-// C O L L A P S E - U M S T Y L E R ???
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
 const OneRecipeBig = ({recipesData}) => {
   const {_id} = useParams();
   const currRecipe = recipesData.find(recipe => recipe._id === _id);
@@ -44,14 +26,12 @@ const OneRecipeBig = ({recipesData}) => {
 
   return (
     <>
-
       <div >
         <h1 >{currRecipe.name}</h1>
         <img src={currRecipe.img} alt={currRecipe.name} />
         <h3>description</h3>
         <p >{currRecipe.description}n</p>
       </div>
-
       <div className="flex">
         <h3>ingredients</h3>
         <div 
@@ -61,7 +41,6 @@ const OneRecipeBig = ({recipesData}) => {
       <div className={!ingredientsToggle ? "showIngredients" : ""}>
         <p >{currRecipe.ingredients}</p>
       </div>
-
       <div className="flex">
         <h3>preparation</h3>
         <div 
@@ -71,14 +50,10 @@ const OneRecipeBig = ({recipesData}) => {
       <div className={!preparationsToggle ? "showPreparations" : ""}>
         <p >{currRecipe.preparation}</p>
       </div>
-
-
       <div className="options">
         {/* <Link to="/one-recipe-big"><button>update</button></Link> */}
         <Link to="/all-recipes">{ <AiOutlineCloseCircle className="icon"/> }</Link>
       </div>
-
-
     </>
   );
 }
